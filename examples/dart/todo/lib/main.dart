1eb667250c293815878485a0ddbc52ca3c56569d
Thorsten Lorenz <thlorenz@gmx.de>
2021-03-06 09:15:16 -0500

    example: todo update (debug, complete)
    - exposing `debug(pretty)` method on models that derive `Debug`
    - adding `complete|restart` messages which pass `id` of todo to modify
      it

--- examples/dart/todo/lib/main.dart
+++ examples/dart/todo/lib/main.dart
@@ -1,15 +1,28 @@
 import 'generated/rid_generated.dart';
+import 'dart:io';
+
+const LOG_VERBOSE = true;
 
 messages() {
   final model = rid_ffi.init_model_ptr();
-  model.msgAddTodo(1);
-  model.msgAddTodo(4);
+  model.msgAddTodo("Hello");
+  model.msgAddTodo("World");
 
-  final allTodos = model.todos.iter().map((x) => x.id);
-  print("""
-todos len:  ${model.todos.length}
-todos:      [ ${allTodos.join(', ')} ]
-""");
+  print(model.debug(LOG_VERBOSE));
+
+  model.msgCompleteTodo(1);
+  print(model.debug(LOG_VERBOSE));
+
+  model.msgRestartTodo(1);
+  print(model.debug(LOG_VERBOSE));
+}
+
+interactive() {
+  final model = rid_ffi.init_model_ptr();
+  for (int i = 0; i < 100; i++) {
+    stdin.readLineSync();
+    model.msgAddTodo("todo_$i");
+  }
 }
 
 main() {
--- examples/dart/todo/src/lib.rs
+++ examples/dart/todo/src/lib.rs
@@ -1,33 +1,56 @@
 #[rid::model]
 #[derive(Debug)]
 pub struct Model {
+    last_added_id: u32,
     todos: Vec<Todo>,
+}
+
+#[rid::model]
+#[derive(Debug)]
+pub struct Todo {
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
+#[rid::message(to = "Model")]
+pub enum Msg {
+    AddTodo(String),
+    CompleteTodo(u32),
+    RestartTodo(u32),
 }
 
 impl Model {
     fn update(&mut self, msg: Msg) {
+        use Msg::*;
         match msg {
-            Msg::AddTodo(id) => {
-                self.todos.push(Todo { id });
+            AddTodo(title) => {
+                self.last_added_id += 1;
+                let todo = Todo {
+                    id: self.last_added_id,
+                    title,
+                    completed: false,
+                };
+                self.todos.push(todo);
             }
+            CompleteTodo(id) => self.update_todo(id, |todo| todo.completed = true),
+            RestartTodo(id) => self.update_todo(id, |todo| todo.completed = false),
+        };
+    }
+
+    fn update_todo<F: FnOnce(&mut Todo)>(&mut self, id: u32, update: F) {
+        match self.todos.iter_mut().find(|x| x.id == id) {
+            Some(todo) => update(todo),
+            None => eprintln!("Could not find Todo with id '{}'", id),
         };
     }
 }
 
 #[no_mangle]
 pub extern "C" fn init_model_ptr() -> *const Model {
-    let model = Model { todos: vec![] };
+    let model = Model {
+        last_added_id: 0,
+        todos: vec![],
+    };
     Box::into_raw(Box::new(model))
-}
-
-#[rid::model]
-#[derive(Debug)]
-pub struct Todo {
-    id: u8,
-}
-
-#[rid::message(to = "Model")]
-#[derive(Debug)]
-pub enum Msg {
-    AddTodo(u8),
 }
