96bef27d42968bc4781170cddc83f0f980ed7195
Thorsten Lorenz <thlorenz@gmx.de>
2021-04-17 10:03:01 -0600

    rid-macro: renamed type-info module

--- rid-macro-impl/src/attrs/mod.rs
+++ rid-macro-impl/src/attrs/mod.rs
@@ -3,14 +3,14 @@ pub use config_function::*;
 pub use config_impl_block::*;
 pub use config_struct::*;
 pub use parse_attrs::*;
-pub use type_category::*;
+pub use type_info::*;
 
 mod config_enum;
 mod config_function;
 mod config_impl_block;
 mod config_struct;
 mod parse_attrs;
-mod type_category;
+mod type_info;
 
 #[cfg(test)]
 pub mod parse_attrs_test;
