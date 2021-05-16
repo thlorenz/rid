871bb3d97b2ee11f92f1f854ecd82bd7af122c5d
Thorsten Lorenz <thlorenz@gmx.de>
2021-05-16 16:36:16 -0600

    clock: working version return string async
    - using allo-isolate, tokio crates

--- examples/dart/clock/lib/main.dart
+++ examples/dart/clock/lib/main.dart
@@ -1,3 +1,71 @@
-void main() {
-  print('TODO implement clock');
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:io';
+import 'package:clock/generated/ffigen_binding.dart';
+import 'package:clock/generated/rid_generated.dart';
+import 'package:isolate/ports.dart';
+
+///
+/// Rid generated dynamic library open and export
+///
+DynamicLibrary _open() {
+  if (Platform.isLinux)
+    return DynamicLibrary.open(
+        '/Volumes/d/dev/fluster/rid/rid/examples/dart/clock/../../../target/debug/libclock.so');
+  if (Platform.isMacOS)
+    return DynamicLibrary.open(
+        '/Volumes/d/dev/fluster/rid/rid/examples/dart/clock/../../../target/debug/libclock.dylib');
+  throw UnsupportedError(
+      'Platform "${Platform.operatingSystem}" is not supported.');
+}
+
+final DynamicLibrary _dl = _open();
+final rid_ffi = NativeLibrary(_dl);
+
+///
+/// Binding to `allo-isolate` crate
+///
+void store_dart_post_cobject(
+  Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>> ptr,
+) {
+  _store_dart_post_cobject(ptr);
+}
+
+final _store_dart_post_cobject_Dart _store_dart_post_cobject = _dl
+    .lookupFunction<_store_dart_post_cobject_C, _store_dart_post_cobject_Dart>(
+        'store_dart_post_cobject');
+typedef _store_dart_post_cobject_C = Void Function(
+  Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>> ptr,
+);
+typedef _store_dart_post_cobject_Dart = void Function(
+  Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>> ptr,
+);
+
+///
+/// App
+///
+class Test {
+  static setup() {
+    store_dart_post_cobject(NativeApi.postCObject);
+    print('setup done');
+  }
+
+  Future<String> loadPage(String url) {
+    // defined in package:ffi/ffi.dart
+    final urlPtr = url.toNativeInt8();
+    final completer = Completer<String>();
+    final sendPort = singleCompletePort(completer);
+    final res = rid_ffi.load_page(sendPort.nativePort, urlPtr);
+    if (res != 1) {
+      print("ERROR when initializing page load");
+    }
+    return completer.future;
+  }
+}
+
+Future<void> main() async {
+  Test.setup();
+  final test = Test();
+  final res = await test.loadPage("https://github.com");
+  print("Response: $res");
 }
--- examples/dart/clock/src/lib.rs
+++ examples/dart/clock/src/lib.rs
@@ -1,7 +1,55 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use allo_isolate::Isolate;
+use std::{error::Error, ffi::CStr, fmt};
+
+use lazy_static::lazy_static;
+use tokio::runtime::{Builder, Runtime};
+
+// -----------------
+// Test Error
+// -----------------
+#[derive(Copy, Clone, Debug)]
+pub struct TestError;
+impl fmt::Display for TestError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error while testing.")
     }
+}
+impl Error for TestError {}
+
+// -----------------
+// Tokio Runtime
+// -----------------
+lazy_static! {
+    static ref RUNTIME: ::std::io::Result<Runtime> = Builder::new()
+        .threaded_scheduler()
+        .enable_all()
+        .core_threads(4)
+        .thread_name("flutterust")
+        .build();
+}
+
+pub async fn load_page_impl(url: &str) -> Result<String, TestError> {
+    Ok(format!("loaded {}", url))
+}
+
+// -----------------
+// load_page ffi wrapper
+// -----------------
+#[no_mangle]
+pub extern "C" fn load_page(
+    port: i64,
+    url: *const ::std::os::raw::c_char,
+) -> i32 {
+    let runtime = match RUNTIME.as_ref() {
+        Ok(x) => x,
+        Err(err) => {
+            eprintln!("Err: {:?}", err);
+            return 0;
+        }
+    };
+    let url = unsafe { CStr::from_ptr(url).to_str().unwrap() };
+
+    let t = Isolate::new(port).task(load_page_impl(url));
+    runtime.spawn(t);
+    1
 }
