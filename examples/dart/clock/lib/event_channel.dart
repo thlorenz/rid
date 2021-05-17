cf09c7691444e4eabc252f222c58f75ce7fac79d
Thorsten Lorenz <thlorenz@gmx.de>
2021-05-16 23:07:23 -0600

    clock: event channel properly zoning callbacks

--- examples/dart/clock/lib/event_channel.dart
+++ examples/dart/clock/lib/event_channel.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
 import 'dart:ffi';
 import 'dart:isolate';
 
 import 'package:clock/isolate_binding.dart';
 
 class EventChannel<TEvent> {
-  final List<void Function(TEvent)> _subscriptions = List.empty(growable: true);
+  final List<ZoneUnaryCallback<void, TEvent>> _subscriptions =
+      List.empty(growable: true);
+
+  final _zone = Zone.current;
+
   late final RawReceivePort receivePort;
 
   EventChannel() {
@@ -12,12 +17,13 @@ class EventChannel<TEvent> {
   }
 
   void subscribe(void Function(TEvent) cb) {
-    _subscriptions.add(cb);
+    final zoned = _zone.registerUnaryCallback(cb);
+    _subscriptions.add(zoned);
   }
 
   void _onReceivedResponse(TEvent response) {
-    for (final cb in _subscriptions) {
-      cb(response);
+    for (final zoned in _subscriptions) {
+      _zone.runUnary(zoned, response);
     }
   }
 
