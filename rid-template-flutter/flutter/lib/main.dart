c74a9c0b479a8dd16de5366c7dc2f73ce55c9bc0
Thorsten Lorenz <thlorenz@gmx.de>
2021-06-04 21:10:56 -0600

    rid-template: adapting flutter template to latest API

--- rid-template-flutter/flutter/lib/main.dart
+++ rid-template-flutter/flutter/lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:plugin/plugin.dart';
 
-void main() => runApp(MyApp());
+/// Locks down the store while building widgets in order to prevent the
+/// application from modifying its state while we are reading it in order to
+/// render the widget.
+/// For this app this wouldn't be necessary since no background tasks that
+/// write to the store are running. However it is good practice to do this anyways.
+/// Note that when using StateManagement solutions like riverpod, rid will
+/// ensure the above by other means.
+void syncStoreAccess() {
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  binding.addPersistentFrameCallback((_) {
+    rid_ffi.rid_store_lock();
+    binding.addPostFrameCallback((_) {
+      rid_ffi.rid_store_unlock();
+    });
+  });
+}
+
+void main() {
+  final store = rid_ffi.createStore();
+  syncStoreAccess();
+  runApp(MyApp(store));
+}
 
 class MyApp extends StatelessWidget {
+  final Pointer<Store> _store;
+
+  const MyApp(this._store, {Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,21 +36,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Rust/Flutter Counter Page'),
+      home: MyHomePage(_store, title: 'Rust/Flutter Counter Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final Pointer<Store> _store;
+  MyHomePage(this._store, {Key? key, required this.title}) : super(key: key);
   final String title;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _model = rid_ffi.initModel();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have counted to:',
             ),
             Text(
-              '${_model.count}',
+              '${widget._store.count}',
               style: Theme.of(context).textTheme.headline4,
             ),
             const SizedBox(height: 100),
@@ -51,7 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: _addTen,
+            onPressed: () {
+              _addTen();
+            },
             tooltip: 'Add 10',
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -67,17 +93,18 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _addTen() {
-    setState(() {
-      _model.msgAdd(10);
-    });
-    debugPrint("${_model.debug(true)}");
+  void _addTen() async {
+    final res = await widget._store.msgAdd(10);
+    debugPrint('$res');
+    debugPrint("${widget._store.debug(true)}");
+    setState(() {});
   }
 
   void _incrementCounter() {
-    setState(() {
-      _model.msgInc();
+    widget._store.msgInc().then((res) {
+      debugPrint('$res');
+      debugPrint("${widget._store.debug(true)}");
+      setState(() {});
     });
-    debugPrint("${_model.debug()}");
   }
 }
--- rid-template-flutter/rust/src/lib.rs
+++ rid-template-flutter/rust/src/lib.rs
@@ -1,27 +1,37 @@
 #[rid::model]
 #[derive(Debug, rid::Debug)]
-pub struct Model {
+pub struct Store {
     count: u32,
 }
 
-#[rid::export]
-impl Model {
-    #[rid::export(initModel)]
-    fn new() -> Self {
+impl Store {
+    fn create() -> Self {
         Self { count: 0 }
     }
 
-    fn update(&mut self, msg: Msg) {
+    fn update(&mut self, req_id: u64, msg: Msg) {
         match msg {
-            Msg::Inc => self.count += 1,
-            Msg::Add(n) => self.count += n,
+            Msg::Inc => {
+                self.count += 1;
+                rid::post(Reply::Icreased(req_id));
+            }
+            Msg::Add(n) => {
+                self.count += n;
+                rid::post(Reply::Added(req_id, n.to_string()));
+            }
         }
     }
 }
 
-#[rid::message(Model)]
+#[rid::message(Store, Reply)]
 #[derive(Debug)]
 pub enum Msg {
     Inc,
     Add(u32),
+}
+
+#[rid::reply]
+pub enum Reply {
+    Icreased(u64),
+    Added(u64, String),
 }
