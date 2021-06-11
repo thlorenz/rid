714e74759ce0de236fd4a960ee1926fb28fca92d
Thorsten Lorenz <thlorenz@gmx.de>
2021-06-11 01:18:15 -0500

    rid-macro: test utils improvments

--- rid-macro-impl/src/common/errors.rs
+++ rid-macro-impl/src/common/errors.rs
@@ -1,18 +1,38 @@
-use proc_macro2::Span;
+use proc_macro2::{Span, TokenStream};
 use syn::spanned::Spanned;
 
-type Tokens = proc_macro2::TokenStream;
-pub fn type_error(ty: &syn::Type, err: &String) -> Tokens {
+use crate::common::abort;
+
+pub fn type_error(ty: &syn::Type, err: &String) -> TokenStream {
     let full_err = format!("rid: {}", err);
     syn::Error::new(ty.span(), full_err).to_compile_error()
 }
 
-pub fn derive_error(ident: &syn::Ident, err: &str) -> Tokens {
+pub fn derive_error(ident: &syn::Ident, err: &str) -> TokenStream {
     let full_err = format!("rid: {}", err);
     syn::Error::new(ident.span(), full_err).to_compile_error()
 }
 
-pub fn callsite_error(err: &str) -> Tokens {
+pub fn callsite_error(err: &str) -> TokenStream {
     let full_err = format!("rid: {}", err);
     syn::Error::new(Span::call_site(), full_err).to_compile_error()
+}
+
+pub fn missing_struct_enum_info(ident: &syn::Ident) -> TokenStream {
+    abort!(
+        ident,
+        "[rid] Missing info for type {0}.\nSpecify it via one of the below:\n\
+        \x20- #[rid::structs({0})]\n\
+        \x20- #[rid::enums({0})]",
+        ident
+    )
+}
+
+pub fn missing_msg_field_enum_info(ident: &syn::Ident) -> TokenStream {
+    abort!(
+        ident,
+        "[rid] Missing info for type {0}.\nSpecify it via: #[rid::enums({0})]\n\n\
+        Note that for message fields only primitive types, strings and enums are supported.",
+        ident
+    )
 }
--- rid-macro-impl/src/common/test.rs
+++ rid-macro-impl/src/common/test.rs
@@ -9,10 +9,37 @@ pub fn remove_doc_comments(tokens: TokenStream) -> TokenStream {
     without_docs.join("\n").parse().unwrap()
 }
 
-pub fn dump_tokens(tokens: TokenStream) {
-    eprintln!("--------\n\n{}\n\n----------", tokens);
+pub fn dump_tokens(tokens: &TokenStream) {
+    eprintln!("----------\n\n{}\n\n----------", tokens);
 }
 
 pub fn dump_code(code: &str) {
-    eprintln!("--------\n\n{}\n\n----------", code);
+    eprintln!("----------\n\n{}\n\n----------", code);
+}
+
+pub fn normalize_code(code: &str) -> String {
+    code.lines()
+        .filter_map(|x| {
+            let x = x.trim();
+            if x.is_empty() {
+                None
+            } else {
+                Some(x)
+            }
+        })
+        .collect::<Vec<&str>>()
+        .join("\n")
+}
+
+pub fn normalize_code_vec(code: &str) -> Vec<&str> {
+    code.lines()
+        .filter_map(|x| {
+            let x = x.trim();
+            if x.is_empty() {
+                None
+            } else {
+                Some(x)
+            }
+        })
+        .collect::<Vec<&str>>()
 }
