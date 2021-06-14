464100e69f45586985b2ef55daaf148ee1b3832b
Thorsten Lorenz <thlorenz@gmx.de>
2021-06-14 17:29:01 -0600

    rid-macro: toDart for vec takes `autoDispose` as property

--- rid-macro-impl/src/render_dart/vec.dart
+++ rid-macro-impl/src/render_dart/vec.dart
@@ -8,13 +8,13 @@
 ///   }
 ///
 ///   /// **WARNING**: You cannot use this Vec pointer anymore after this call
-///   /// completes unless you pass [false] as [autoDispose].
+///   /// completes unless you set [autoDispose] to [false].
 ///   ///
 ///   /// Converts this Vec pointer into a Dart [List&lt;{dart_item_type}&gt;] and disposes the
-///   /// underlying Rust Vec unless [false] is passed as [autoDispose].
+///   /// underlying Rust Vec unless [autoDispose] is set to [false].
 ///   /// As a result if [autoDispose] is [true] you cannot use the underlying
 ///   /// Vec anymore after this call completes.
-///   List<{dart_item_type}> toDart([bool autoDispose = true]) {
+///   List<{dart_item_type}> toDart({bool autoDispose = true}) {
 ///     ridStoreLock();
 ///     final list = this.iter().map((raw) => raw.toDart()).toList();
 ///     if (autoDispose) dispose();
