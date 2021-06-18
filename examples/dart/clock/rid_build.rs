fa3847f2864c0b09c9b293b8ea9345dcf3f3f219
Thorsten Lorenz <thlorenz@gmx.de>
2021-06-17 22:20:46 -0600

    chore: excluding examples from workspace

--- examples/dart/clock/rid_build.rs
+++ examples/dart/clock/rid_build.rs
@@ -6,7 +6,7 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR")
         .expect("Missing CARGO_MANIFEST_DIR, please run this via 'cargo run'");
 
-    let workspace_dir = &format!("{}/../../../", crate_dir);
+    let workspace_dir = &format!("{}", crate_dir);
 
     let crate_name = &env::var("CARGO_PKG_NAME")
         .expect("Missing CARGO_PKG_NAME, please run this via 'cargo run'");
