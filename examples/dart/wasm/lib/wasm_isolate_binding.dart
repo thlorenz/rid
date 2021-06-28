f5439322acf2a84e654cf7baf3e36b4ee11a83bd
Thorsten Lorenz <thlorenz@gmx.de>
2021-06-27 19:07:18 -0600

    wasm: working interaction avoiding isolate

--- examples/dart/wasm/lib/wasm_isolate_binding.dart
+++ examples/dart/wasm/lib/wasm_isolate_binding.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi' as dart_ffi;
 
+import 'package:wasm/wasm.dart';
 import 'package:wasm_example/wasm_binding.dart';
 
 bool _initializedIsolate = false;
@@ -30,29 +31,23 @@ typedef _c_rid_init_isolate = dart_ffi.Void Function(
   dart_ffi.Int64 port,
 );
 
-typedef _dart_rid_init_isolate = void Function(
-  int port,
-);
-
 void initIsolate(
   WasmLibrary dl,
   int port,
   bool isDebugMode,
 ) {
   // allo isolate crate initialization
-  final _store_dart_post_cobject_Dart _store_dart_post_cobject =
-      dl.lookupFunction<_store_dart_post_cobject_C,
-          _store_dart_post_cobject_Dart>('store_dart_post_cobject');
+  final WasmFunction _store_dart_post_cobject = dl.lookupFunction<
+      _store_dart_post_cobject_C,
+      _store_dart_post_cobject_Dart>('store_dart_post_cobject');
 
-  final _rid_init_isolate_ptr = dl
+  final WasmFunction _rid_init_isolate = dl
       .lookup<dart_ffi.NativeFunction<_c_rid_init_isolate>>('rid_init_isolate');
-  final _dart_rid_init_isolate _rid_init_isolate =
-      _rid_init_isolate_ptr.asFunction<_dart_rid_init_isolate>();
 
   if (!_initializedIsolate || isDebugMode) {
     _initializedIsolate = true;
-    _store_dart_post_cobject(dart_ffi.NativeApi.postCObject);
-    _rid_init_isolate(port);
+    _store_dart_post_cobject.apply([dart_ffi.NativeApi.postCObject.address]);
+    _rid_init_isolate.apply([port]);
   } else if (_initializedIsolate) {
     throw Exception(
         "The isolate can only be initialized once when not run in debug mode");
