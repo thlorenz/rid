9267e1d726dbeff1ea99557c6967db860b7008d8
Thorsten Lorenz <thlorenz@gmx.de>
2021-06-28 20:51:44 -0600

    wasm: initial adaptation to wasm_interop
    - can't use anything FFI though

--- examples/dart/wasm/lib/main.dart
+++ examples/dart/wasm/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:wasm_example/generated/rid_api.dart';
 
 Future<void> main() async {
+  await initRid();
   final store = Store.instance;
   print('store: ${store.debug(true)}');
 
