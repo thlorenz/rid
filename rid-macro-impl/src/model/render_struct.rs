debb648b2870e50ea55dbdde77d09129b7ac63ec
Thorsten Lorenz <thlorenz@gmx.de>
2021-07-30 19:47:50 -0500

    rid-macro: using new fieldacess implementaion

--- rid-macro-impl/src/model/render_struct.rs
+++ rid-macro-impl/src/model/render_struct.rs
@@ -2,12 +2,9 @@ use super::{debug::render_debug, to_dart::render_to_dart};
 use crate::{
     attrs::{parse_derive_attrs, StructConfig},
     common::abort,
-    model::{
-        parsed_struct::ParsedStruct,
-        store::{render_store_field_wrapper_extension, render_store_module},
-    },
+    model::store::{render_store_field_wrapper_extension, render_store_module},
     parse,
-    parse::rust_type::RustType,
+    parse::{rust_type::RustType, ParsedStruct},
 };
 use proc_macro2::TokenStream;
 use quote::quote_spanned;
@@ -16,7 +13,7 @@ use syn::{Fields, ItemStruct};
 pub fn render_struct(struct_item: &ItemStruct, is_store: bool) -> TokenStream {
     let derive = parse_derive_attrs(&struct_item.attrs);
     let struct_config = StructConfig::from(&struct_item);
-    let parsed_struct = parse::ParsedStruct::new(
+    let parsed_struct = ParsedStruct::new(
         &struct_item,
         &struct_item.ident,
         struct_config.clone(),
@@ -51,16 +48,18 @@ pub fn render_struct(struct_item: &ItemStruct, is_store: bool) -> TokenStream {
     };
 
     // -----------------
-    // rid::export
+    // rid::model field access
     // -----------------
     let exports = match &struct_item.fields {
         Fields::Named(fields) => {
             let parsed_struct = ParsedStruct::new(
-                struct_item.ident.clone(),
-                &fields.named,
-                struct_config,
+                &struct_item,
+                &struct_item.ident,
+                struct_config.clone(),
             );
-            parsed_struct.tokens()
+            let (tokens, _) = parsed_struct
+                .render_field_access(&Default::default(), &Default::default());
+            tokens
         }
         Fields::Unnamed(_) => abort!(
             struct_item.ident,
