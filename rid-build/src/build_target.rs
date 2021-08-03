7bd6c34203efb424e96c758ab8b1e9055a8ae460
Thorsten Lorenz <thlorenz@gmx.de>
2021-08-03 17:48:40 -0500

    rid-build: expanding without cargo expand

--- rid-build/src/build_target.rs
+++ rid-build/src/build_target.rs
@@ -8,15 +8,19 @@ pub enum BuildTarget {
 impl BuildTarget {
     pub(crate) fn cargo_expand_args(&self) -> Vec<String> {
         use BuildTarget::*;
-        match self {
-            Release | Debug => {
-                vec!["expand".to_string(), "--lib".to_string()]
+
+        let mut target = match self {
+            Release | Debug => vec!["--lib".to_string()],
+            DebugExample(example_name) => {
+                vec!["--example".to_string(), example_name.to_string()]
             }
-            DebugExample(example_name) => vec![
-                "expand".to_string(),
-                "--example".to_string(),
-                example_name.to_string(),
-            ],
-        }
+        };
+        let mut cmd = vec!["rustc".to_string()];
+        cmd.append(&mut target);
+        cmd.append(&mut vec![
+            "--".to_string(),
+            "-Zunpretty=expanded".to_string(),
+        ]);
+        cmd
     }
 }
