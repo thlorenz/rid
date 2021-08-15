91c9d0f7cb7076fd276dfe76ac1823164afd61b9
Thorsten Lorenz <thlorenz@gmx.de>
2021-08-15 17:06:08 -0500

    rid-macro: adding access_string_ref to utils module

--- rid-macro-impl/src/common/utils_module_tokens.rs
+++ rid-macro-impl/src/common/utils_module_tokens.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote, quote_spanned};
-use rid_common::{CSTRING_FREE, UTILS_MODULE};
+use rid_common::{CSTRING_FREE, STRING_REF_ACCESS, UTILS_MODULE};
 
 use crate::common::state::{get_state, ImplementationType};
 
@@ -18,9 +18,11 @@ pub fn utils_module_tokens() -> TokenStream {
         .needs_implementation(&ImplementationType::UtilsModule, UTILS_MODULE)
     {
         let cstring_free = cstring_free();
+        let string_ref_access = string_ref_access();
         quote! {
             mod __rid_utils_module {
                 #cstring_free
+                #string_ref_access
             }
         }
     } else {
@@ -28,6 +30,9 @@ pub fn utils_module_tokens() -> TokenStream {
     }
 }
 
+// -----------------
+// CString
+// -----------------
 fn cstring_free() -> TokenStream {
     let cstring_free_ident = format_ident!("{}", CSTRING_FREE);
     quote_spanned! {
@@ -37,6 +42,29 @@ fn cstring_free() -> TokenStream {
             if !ptr.is_null() {
                 ::core::mem::drop(unsafe { ::std::ffi::CString::from_raw(ptr) });
             }
+        }
+    }
+}
+
+// -----------------
+// String
+// -----------------
+fn string_ref_access() -> TokenStream {
+    let string_ref_access_ident = format_ident!("{}", STRING_REF_ACCESS);
+    quote_spanned! {
+        proc_macro2::Span::call_site() =>
+        #[no_mangle]
+        pub extern "C" fn #string_ref_access_ident(
+            ptr: *mut String,
+        ) -> *const ::std::os::raw::c_char {
+            let s: &String = unsafe {
+                assert!(!ptr.is_null());
+                let ptr: *mut String = &mut *ptr;
+                ptr.as_mut().expect("resolve_ptr.as_mut failed")
+            };
+
+            let cstring = ::std::ffi::CString::new(s.as_str()).unwrap();
+            cstring.into_raw()
         }
     }
 }
