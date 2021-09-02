8e463be74ab102526f664d1696bf1300d33ae820
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-02 16:38:05 -0500

    test: testing hash_map key/val int primitives

--- tests/dart/field_access/src/hash_maps.rs
+++ tests/dart/field_access/src/hash_maps.rs
@@ -5,6 +5,9 @@ use rid::RidStore;
 #[rid::store]
 pub struct Store {
     u8s: HashMap<u8, u8>,
+    u32s: HashMap<u32, u32>,
+    i8s: HashMap<i8, i8>,
+    i64s: HashMap<i64, i64>,
 }
 
 impl RidStore<Msg> for Store {
@@ -13,7 +16,27 @@ impl RidStore<Msg> for Store {
         u8s.insert(1, 11);
         u8s.insert(2, 22);
         u8s.insert(3, 33);
-        Self { u8s }
+
+        let mut u32s = HashMap::new();
+        u32s.insert(11, 111);
+        u32s.insert(22, 222);
+        u32s.insert(33, 333);
+
+        let mut i8s = HashMap::new();
+        i8s.insert(-1, -11);
+        i8s.insert(-2, -22);
+        i8s.insert(-3, -33);
+
+        let mut i64s = HashMap::new();
+        i64s.insert(-11, -111);
+        i64s.insert(-22, -222);
+        i64s.insert(-33, -333);
+        Self {
+            u8s,
+            u32s,
+            i8s,
+            i64s,
+        }
     }
 
     fn update(&mut self, _req_id: u64, _msg: Msg) {
--- tests/dart/field_access/test/hash_maps.dart
+++ tests/dart/field_access/test/hash_maps.dart
@@ -3,6 +3,16 @@ import 'package:test/test.dart';
 import '../lib/generated/rid_api.dart';
 
 void main() {
+  RID_DEBUG_LOCK = null;
+  RID_DEBUG_REPLY = null;
+
+  // -----------------
+  // Primitives key/val same type
+  // -----------------
+
+  // -----------------
+  // unsigned ints
+  // -----------------
   test('field_access: raw HashMap<u8, u8>', () {
     final store = Store.instance;
 
@@ -21,16 +31,61 @@ void main() {
 
   test('field_access: HashMap<u8, u8>', () {
     final store = Store.instance;
-
-    expect(store.u8s.length, 3, reason: 'u8s len');
-    expect(store.u8s.containsKey(1), true, reason: 'u8s containsKey 1 -> true');
-    expect(store.u8s.containsKey(2), true, reason: 'u8s containsKey 2 -> true');
-    expect(store.u8s.containsKey(5), false,
-        reason: 'u8s containsKey 5 -> false');
-
     final u8s = store.u8s;
+
+    expect(u8s.length, 3, reason: 'u8s len');
+    expect(u8s.containsKey(1), true, reason: 'u8s containsKey 1 -> true');
+    expect(u8s.containsKey(2), true, reason: 'u8s containsKey 2 -> true');
+    expect(u8s.containsKey(5), false, reason: 'u8s containsKey 5 -> false');
+
     expect(u8s[1], 11, reason: 'u8s get(1)');
     expect(u8s[3], 33, reason: 'u8s get(3)');
     expect(u8s[5], null, reason: 'u8s get(5) -> null');
+  });
+
+  test('field_access: HashMap<u32, u32>', () {
+    final store = Store.instance;
+    final u32s = store.u32s;
+
+    expect(u32s.length, 3, reason: 'u32s len');
+    expect(u32s.containsKey(11), true, reason: 'u32s containsKey 11 -> true');
+    expect(u32s.containsKey(22), true, reason: 'u32s containsKey 22 -> true');
+    expect(u32s.containsKey(55), false, reason: 'u32s containsKey 55 -> false');
+
+    expect(u32s[11], 111, reason: 'u32s get(11)');
+    expect(u32s[33], 333, reason: 'u32s get(33)');
+    expect(u32s[55], null, reason: 'u32s get(5) -> null');
+  });
+
+  // -----------------
+  // signed ints
+  // -----------------
+  test('field_access: HashMap<i8, i8>', () {
+    final store = Store.instance;
+    final i8s = store.i8s;
+
+    expect(i8s.length, 3, reason: 'i8s len');
+    expect(i8s.containsKey(-1), true, reason: 'i8s containsKey -1 -> true');
+    expect(i8s.containsKey(-2), true, reason: 'i8s containsKey -2 -> true');
+    expect(i8s.containsKey(-5), false, reason: 'i8s containsKey -5 -> false');
+
+    expect(i8s[-1], -11, reason: 'i8s get(-1)');
+    expect(i8s[-3], -33, reason: 'i8s get(-3)');
+    expect(i8s[-5], null, reason: 'i8s get(-5) -> null');
+  });
+
+  test('field_access: HashMap<i64, i64>', () {
+    final store = Store.instance;
+    final i64s = store.i64s;
+
+    expect(i64s.length, 3, reason: 'i64s len');
+    expect(i64s.containsKey(-11), true, reason: 'i64s containsKey -11 -> true');
+    expect(i64s.containsKey(-22), true, reason: 'i64s containsKey -22 -> true');
+    expect(i64s.containsKey(-55), false,
+        reason: 'i64s containsKey -55 -> false');
+
+    expect(i64s[-11], -111, reason: 'i64s get(-11)');
+    expect(i64s[-33], -333, reason: 'i64s get(-33)');
+    expect(i64s[-55], null, reason: 'i64s get-(55) -> null');
   });
 }
