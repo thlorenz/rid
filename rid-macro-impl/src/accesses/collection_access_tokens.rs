6cf8748b027ac99c34fa7f8d41a6e30b85e479ab
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-03 18:38:13 -0500

    rid-macro: extracting collection access tokens from vec access method
    - needed to make it easier useable for hash maps
    - named collection_access_tokens for lack of better idea of what to name
      it for now

---
+++ rid-macro-impl/src/accesses/collection_access_tokens.rs
@@ -0,0 +1,82 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::Ident;
+
+use crate::{
+    parse::rust_type::RustType,
+    render_common::PointerTypeAlias,
+    render_rust::{render_return_type, RenderedReturnType},
+};
+
+use super::AccessKind;
+
+pub struct CollectionAccessTokens {
+    /// The tokens for the type that should be returned by the get item wrapper method
+    pub item_return_type: TokenStream,
+
+    /// The tokens to convert the type of the wrapped method into the one the wrapper returns
+    pub into_return_type: TokenStream,
+
+    /// Type aliases used as a part of the return type
+    pub type_alias: Option<PointerTypeAlias>,
+}
+
+/// Helper function to figure out the return type and into return type tokens for
+/// a specific collection type. Used for `Vec` get by index and `HashMap` get by key.
+pub fn collection_access_tokens(
+    ptr_ident: Ident,
+    item_type: &RustType,
+    access_kind: &AccessKind,
+) -> CollectionAccessTokens {
+    // CString and str aren't FFI safe so we send the item content as a *char.
+    // For consistency we do the same for Strings.
+    let (item_return_type, into_return_type, type_alias) = if item_type
+        .is_string_like()
+    {
+        let item_return_type = quote! { *const ::std::os::raw::c_char };
+        let into_return_type = if item_type.is_string() {
+            quote! {
+                let s: &String = unsafe {
+                    assert!(!#ptr_ident.is_null());
+                    let #ptr_ident: *mut String = #ptr_ident as *mut String;
+                    #ptr_ident.as_mut().expect("resolve ptr from collection item as_mut failed")
+                };
+                let cstring = ::std::ffi::CString::new(s.as_str()).unwrap();
+                cstring.into_raw()
+            }
+        } else if item_type.is_cstring() {
+            quote! {
+                let cstring: &::std::ffi::CString = unsafe {
+                    assert!(!#ptr_ident.is_null());
+                    let #ptr_ident: *mut ::std::ffi::CString = #ptr_ident as *mut ::std::ffi::CString;
+                    #ptr_ident.as_mut().expect("resolve_ptr.as_mut failed")
+                };
+                cstring.clone().into_raw()
+            }
+        } else if item_type.is_str() {
+            quote! {
+                let s: &str = unsafe {
+                    assert!(!#ptr_ident.is_null());
+                    let #ptr_ident: *mut str = #ptr_ident as *mut str;
+                    #ptr_ident.as_mut()
+                        .expect("resolve ptr from collection item as_mut failed")
+                };
+                let cstring = ::std::ffi::CString::new(s).unwrap();
+                cstring.into_raw()
+            }
+        } else {
+            panic!("Should have covered all string-like cases")
+        };
+        (item_return_type, into_return_type, None)
+    } else {
+        let RenderedReturnType {
+            tokens, type_alias, ..
+        } = render_return_type(&item_type, access_kind);
+        (tokens, quote! { #ptr_ident }, type_alias)
+    };
+    CollectionAccessTokens {
+        item_return_type,
+        into_return_type,
+        type_alias,
+    }
+}
--- rid-macro-impl/src/render_rust/render_access_item.rs
+++ rid-macro-impl/src/render_rust/render_access_item.rs
@@ -4,7 +4,7 @@ use quote::{format_ident, quote, quote_spanned};
 use syn::Ident;
 
 use crate::{
-    accesses::AccessKind,
+    accesses::{collection_access_tokens, AccessKind, CollectionAccessTokens},
     parse::{
         rust_type::{Composite, Primitive, RustType, TypeKind, Value},
         ParsedFunction, ParsedReference,
@@ -92,52 +92,11 @@ fn render_vec_access_item(
         ..
     } = render_return_type(outer_type, access_kind);
 
-    // CString and str aren't FFI safe so we send the vec item content as a *char.
-    // For consistency we do the same for Strings.
-    let (item_return_type, into_return_type, type_alias) = if item_type
-        .is_string_like()
-    {
-        let item_return_type = quote! { *const ::std::os::raw::c_char };
-        let into_return_type = if item_type.is_string() {
-            quote! {
-                let s: &String = unsafe {
-                    assert!(!ptr.is_null());
-                    let ptr: *mut String = ptr as *mut String;
-                    ptr.as_mut().expect("resolve ptr from vec item as_mut failed")
-                };
-                let cstring = ::std::ffi::CString::new(s.as_str()).unwrap();
-                cstring.into_raw()
-            }
-        } else if item_type.is_cstring() {
-            quote! {
-                let cstring: &::std::ffi::CString = unsafe {
-                    assert!(!ptr.is_null());
-                    let ptr: *mut ::std::ffi::CString = ptr as *mut ::std::ffi::CString;
-                    ptr.as_mut().expect("resolve_ptr.as_mut failed")
-                };
-                cstring.clone().into_raw()
-            }
-        } else if item_type.is_str() {
-            quote! {
-                let s: &str = unsafe {
-                    assert!(!ptr.is_null());
-                    let ptr: *mut str = ptr as *mut str;
-                    ptr.as_mut()
-                        .expect("resolve ptr from vec item as_mut failed")
-                };
-                let cstring = ::std::ffi::CString::new(s).unwrap();
-                cstring.into_raw()
-            }
-        } else {
-            panic!("Should have covered all string-like cases")
-        };
-        (item_return_type, into_return_type, None)
-    } else {
-        let RenderedReturnType {
-            tokens, type_alias, ..
-        } = render_return_type(&item_type, access_kind);
-        (tokens, quote! { ptr }, type_alias)
-    };
+    let CollectionAccessTokens {
+        item_return_type,
+        into_return_type,
+        type_alias,
+    } = collection_access_tokens(format_ident!("ptr"), item_type, access_kind);
 
     let tokens = quote_spanned! { fn_access_ident.span() =>
         fn #fn_access_ident(vec: #vec_arg_type, idx: usize) -> #item_return_type {
