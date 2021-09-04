f569c28944a94e2072c46e63f4a2d5986282d8a3
Thorsten Lorenz <thlorenz@gmx.de>
2021-09-04 08:42:31 -0500

    rid-macro: merged render vec access into VecAccess impl module

--- rid-macro-impl/src/render_rust/vec.rs
+++ rid-macro-impl/src/render_rust/vec.rs
@@ -3,19 +3,26 @@ use std::collections::HashMap;
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote, quote_spanned};
 use render_return_type::RenderedReturnType;
+use syn::Ident;
 
 use crate::{
-    accesses::{AccessKind, RenderedAccessRust, VecAccess},
+    accesses::{
+        collection_access_tokens, AccessKind, CollectionAccessTokens,
+        RenderedAccessRust, VecAccess,
+    },
     common::{abort, tokens::resolve_vec_ptr},
+    parse::rust_type::RustType,
     render_common::PointerTypeAlias,
     render_rust::{render_return_type, render_to_return_type},
 };
 
-use super::{
-    render_access_item, render_free, RenderedAccessItem, RenderedFree,
-};
+use super::{render_free, RenderedFree};
 
 impl VecAccess {
+    // -----------------
+    // MethodReturn
+    // -----------------
+
     // TODO: for now ignoring type aliases as they should have been added already when
     // the function using the vec whose access methods we render here were rendered.
     // If it turns out that we don't need those then we don't have to return them from
@@ -34,15 +41,21 @@ impl VecAccess {
         );
         type_alias.map(|x| type_aliases.insert(x.alias.to_string(), x));
 
-        let RenderedAccessItem {
+        let inner_ty = self
+            .vec_type
+            .inner_composite_type()
+            .expect("Vec should have inner type");
+        let RenderedVecAccessItem {
             tokens: access_tokens,
             type_alias,
-        } = render_access_item(
+        } = render_vec_access_item(
             &self.vec_type,
+            &inner_ty,
             &self.fn_get_ident,
-            &self.rust_ffi_prelude,
             &AccessKind::MethodReturn,
+            &self.rust_ffi_prelude,
         );
+
         type_alias.map(|x| type_aliases.insert(x.alias.to_string(), x));
 
         let tokens = quote! {
@@ -54,6 +67,10 @@ impl VecAccess {
             type_aliases,
         }
     }
+
+    // -----------------
+    // FieldAccess
+    // -----------------
 
     /// Main difference to [VecAccess::render_rust] is that we work with references to a vec
     /// that is attached to a model. Thus we do not need `free` and don't wrap inside
@@ -154,4 +171,44 @@ impl VecAccess {
             type_aliases: HashMap::new(),
         }
     }
+}
+
+// -----------------
+// render_vec_access_item
+// -----------------
+struct RenderedVecAccessItem {
+    pub tokens: TokenStream,
+    pub type_alias: Option<PointerTypeAlias>,
+}
+
+fn render_vec_access_item(
+    outer_type: &RustType,
+    item_type: &RustType,
+    fn_access_ident: &Ident,
+    access_kind: &AccessKind,
+    ffi_prelude: &TokenStream,
+) -> RenderedVecAccessItem {
+    let RenderedReturnType {
+        tokens: vec_arg_type,
+        ..
+    } = render_return_type(outer_type, access_kind);
+
+    let CollectionAccessTokens {
+        item_return_type,
+        into_return_type,
+        type_alias,
+    } = collection_access_tokens(format_ident!("ptr"), item_type, access_kind);
+
+    let access_fn = quote_spanned! { fn_access_ident.span() =>
+        fn #fn_access_ident(vec: #vec_arg_type, idx: usize) -> #item_return_type {
+            let ptr = vec[idx];
+            #into_return_type
+
+        }
+    };
+    let tokens = quote! {
+        #ffi_prelude
+        #access_fn
+    };
+    RenderedVecAccessItem { tokens, type_alias }
 }
