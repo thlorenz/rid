f3eae68ae2c9f4358a41119418530f9146940c35
Matias Piipari <matias.piipari@gmail.com>
2021-10-16 07:45:53 +0300

    Made host_props.rs not so gross again with constants for actual constants.

--- rid-build/src/ffigen/host_props.rs
+++ rid-build/src/ffigen/host_props.rs
@@ -6,41 +6,28 @@ pub struct HostProps {
     pub llvm_paths: Vec<String>,
 }
 
+const LINUX_LLVM_PATHS: [&str; 7] = [
+    "/usr/lib/llvm-6.0/lib/libclang.so",
+    "/usr/lib/llvm-9/lib/libclang.so",
+    "/usr/lib/llvm-10/lib/libclang.so",
+    "/usr/lib/llvm-11/lib/libclang.so",
+    "/usr/lib/libclang.so",
+    "/usr/lib64/libclang.so",
+    "/usr/lib/llvm-6.0/lib/libclang.so"
+];
+
+const MACOS_LLVM_PATHS: [&str; 2] = [
+    "/usr/local/opt/llvm/lib/",
+    "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr"];
+
+const WINDOWS_LLVM_PATHS: [&str; 1] = [r#"C:\Program Files\LLVM\bin\"#];
+
 impl HostProps {
     pub fn new() -> Self {
-        let linux_llvm_paths = [
-            "/usr/lib/llvm-6.0/lib/libclang.so".to_owned(),
-            "/usr/lib/llvm-9/lib/libclang.so".to_owned(),
-            "/usr/lib/llvm-10/lib/libclang.so".to_owned(),
-            "/usr/lib/llvm-11/lib/libclang.so".to_owned(),
-            "/usr/lib/libclang.so".to_owned(),
-            "/usr/lib64/libclang.so".to_owned(),
-            "/usr/lib/llvm-6.0/lib/libclang.so".to_owned()
-        ];
-
-        let mut macos_llvm_paths = [
-            "/usr/local/opt/llvm/lib/".to_owned(),
-            "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr".to_owned()].to_vec();
-
-        let windows_llvm_paths = [r#"C:\Program Files\LLVM\bin\"#.to_owned()];
-
-        match home_dir() {
-            None => (),
-            Some(mut path) => {
-                path.push("homebrew/opt/llvm");
-                match path.into_os_string().into_string() {
-                    Err(_) => (),
-                    Ok(path) => {
-                        macos_llvm_paths.push(path.to_owned())
-                    }
-                }
-            }
-        }
-
-        let llvm_paths: Vec<String> = match env::consts::OS {
-            "linux" => linux_llvm_paths.to_vec(),
-            "macos" => macos_llvm_paths.to_vec(),
-            "windows" => windows_llvm_paths.to_vec(),
+        let mut llvm_paths: Vec<String> = match env::consts::OS {
+            "linux" => LINUX_LLVM_PATHS.map(String::from).to_vec(),
+            "macos" => MACOS_LLVM_PATHS.map(String::from).to_vec(),
+            "windows" => WINDOWS_LLVM_PATHS.map(String::from).to_vec(),
             "android" | "ios" | "freebsd" | "dragonfly" | "netbsd"
             | "openbsd" | "solaris" | _ => {
                 panic!("rid cli cannot run on {}", env::consts::OS)
@@ -48,8 +35,22 @@ impl HostProps {
         };
 
         match env::var("LIBCLANG_PATH") {
-            Err(_) => Self {
-                llvm_paths: llvm_paths
+            Err(_) => {
+                match (env::consts::OS, home_dir()) {
+                    ("macos", Some(mut path)) => {
+                        path.push("homebrew/opt/llvm");
+                        match path.into_os_string().into_string() {
+                            Err(_) => (),
+                            Ok(path) => {
+                                llvm_paths.push(path.to_owned());
+                            }
+                        }
+                    },
+                    (_, _) => ()
+                };
+                Self {
+                    llvm_paths: llvm_paths
+                }
             },
             Ok(path) => { 
                 Self {
